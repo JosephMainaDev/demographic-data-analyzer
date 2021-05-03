71890ec4002bf8450f105dd1825376059ea6c129
jmkimani <mainajkimani@gmail.com>
2021-05-03 11:00:40 +0300

    using value_counts method

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,18 +1,23 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
-    # Read data from file
-    df = None
+    # Read adult data from the csv file into a pandas DataFrame object
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    # Select the "race" column and count the values, return a pandas series.
+    # df["race"] == df.race
+    # df.value_counts(subset=["race"]) will also work.
+    race_count = df.value_counts(df["race"])
 
     # What is the average age of men?
-    average_age_men = None
+    # Select "sex" column with value of "Male", and return mean() of "age" column
+    average_age_men = df.loc[df["sex"] == "Male", "age"].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    # b = df.value_counts(df["education"] == "Bachelors", normalize=True)
+    # percentage_bachelors = b[True] * 100
+    percentage_bachelors = len(df.loc[df["education"] == "Bachelors"]) * 100 / len(df)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
